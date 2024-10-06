192d1529096f3f99a179168199ca14004a6d01f7
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-10-07 02:17:21 +0300

    add config

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const BOT_API_KEY = '<Telegram bot api token>';
+export const HELLO_MESSAGE = 'Привет! Я ваш помощник в борьбе с дезинформацией. \n' +
+	'  Отправьте мне новость, и я проверю её на достоверность. \n' +
+	'  Вместе мы сделаем интернет чище и безопаснее! 🌐';
+export const HELLO_STIKER_ID = 'AAMCAgADGQEAAwtnAxosgz2iTSoO6YY1ErWduK_K4gACiAADRA3PF0WNbyw815f-AQAHbQADNgQ';
