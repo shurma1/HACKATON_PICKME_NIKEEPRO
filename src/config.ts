0acd3f75b79ca1ac4654eaa69b3b2d33d7471dd3
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-10-07 02:23:14 +0300

    add api analysis params

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,9 @@ export const HELLO_MESSAGE = 'Привет! Я ваш помощник в бор
 	'  Отправьте мне новость, и я проверю её на достоверность. \n' +
 	'  Вместе мы сделаем интернет чище и безопаснее! 🌐';
 export const HELLO_STIKER_ID = 'AAMCAgADGQEAAwtnAxosgz2iTSoO6YY1ErWduK_K4gACiAADRA3PF0WNbyw815f-AQAHbQADNgQ';
+
+export const API_ANALYSIS_PARAMS = {
+	trust: 1,
+	resourceQuality: 0.5,
+	logic: 1
+}
