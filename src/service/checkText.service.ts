b36572e4886ae792d74e70b9af1bd20d484bacd4
shurma1 <dmitriy.prokhorov.04@mail.ru>
2024-10-07 02:29:43 +0300

    add check text service

---
+++ src/service/checkText.service.ts
@@ -0,0 +1,15 @@
+import {API_ANALYSIS_PARAMS, API_URL} from "../config";
+import axios from "axios";
+import {IDeepTextAnalyse} from "../types/IDeepTextAnalyse";
+
+class CheckTextService {
+	static async Check(text: string): Promise<IDeepTextAnalyse> {
+		return await axios.post(
+			API_URL + '/reliability/check',
+			{
+				text,
+				...API_ANALYSIS_PARAMS
+			}
+		)
+	}
+}
